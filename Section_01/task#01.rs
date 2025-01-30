b521bc16671349d6131361269f50df091fafb7c4
franky <frankneonbluelight@gmail.com>
2025-01-30 13:57:48 -0700

    Create task#01.rs

---
+++ Section_01/task#01.rs
@@ -0,0 +1,15 @@
+// SECTION ONE: SETTING UP THE GAME BOARD AND PLAYER
+//
+// Overview:
+// - Define the board's width and height.
+// - Build out the player, including what it is and how it will look on the board.
+// - Create a game board using a HashMap to represent coordinates (x, y).
+// - Place the player at a specific (x, y) position on the game board.
+//
+// Recap:
+// By the end of this section, we will have a game board with a player displayed on it.
+
+// TASK #01: Define Game Board Dimensions
+// - Use two constants to represent the board's width and height (type: u32).
+// - Since the board dimensions are fixed, use `const` as it ensures the variables remain immutable.
+// - Set their values to your desired board width and height. For example: 10x10, 20x20, etc.
